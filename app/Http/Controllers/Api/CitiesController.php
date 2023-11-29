daf7e4d3b86253123e7a850435a687138d3ceb81
GuiCezaF <contato.guicezafe@gmail.com>
2023-11-29 13:55:14 -0300

    feat: create pagination for get cities

--- app/Http/Controllers/Api/CitiesController.php
+++ app/Http/Controllers/Api/CitiesController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CityResource;
 use App\Models\City;
 use Illuminate\Http\Request;
 
 class CitiesController extends Controller
 {
+
+    public function __construct(protected City $repository,)
+    {
+    }
+
     public function index()
     {
-        return City::all();
+        $cities = $this->repository->paginate();
+
+        return CityResource::collection($cities);
     }
 }
