b31e070245224ffb68c4fd2ce497e2a7d90b9d8f
GuiCezaF <contato.guicezafe@gmail.com>
2023-12-01 13:46:02 -0300

    feat: configure cors

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use App\Http\Controllers\Api\CitiesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/cities', [CitiesController::class, 'index']);
+Route::middleware(['cors'])->group(function () {
+    Route::get('/cities', [CitiesController::class, 'index']);
+});
+
 
 Route::get('/', function () {
     return response()->json([
